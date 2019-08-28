10860ba4f2513f6f617b4d275574b1539d5c1ed0
Auros <41306347+aurosnex@users.noreply.github.com>
2019-08-28 10:30:16 -0400

    Project Setup
    I do not like the BSIPA PluginConfig

---
+++ AlphaTwitch/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Utilities;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace AlphaTwitch
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void Init(IPALogger logger)
+        {
+            Logger.log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+            Logger.log.Debug("OnApplicationStart");
+        }
+
+        public void OnApplicationQuit()
+        {
+            Logger.log.Debug("OnApplicationQuit");
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+
+        }
+    }
+}
